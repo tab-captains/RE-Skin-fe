71c813077e167b5155d04d565ba3cda269196dc1
kiwi13ird <rbf060114@gmail.com>
2025-11-13 20:08:35 +0900

    feat: Navbar 페이지 연결. RootLayout 하위 페이지 렌더링 완료.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import './App.css'
 /* 이곳에 page들 import 후, return 안에 <Route ></Route> 형태로 페이지 연동해 사용. */
 import MainPage from './features/main/pages/MainPage';
 import RootLayout from './features/layout/RootLayout';
+import LoginPage from './features/auth/pages/LoginPage';
+import AnalysisOverviewPage from './features/analysis/pages/AnalysisOverviewPage';
+import CommunityPage from './features/community/pages/CommunityPage';
+
 
 
 function App() {
@@ -10,7 +14,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<RootLayout />}></Route>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={< MainPage/>} />
+          <Route path="analysisOverview" element={<AnalysisOverviewPage />}/>
+          <Route path="login" element={<LoginPage />}/>
+          <Route path="community" element={<CommunityPage />}/>
+      </Route>
       </Routes>
     </Router>
   )
---
+++ src/features/analysis/pages/AnalysisOverviewPage.jsx
@@ -0,0 +1,8 @@
+const AnalysisOverviewPage=()=>{
+  return(
+    <h1>
+      분석미리보기페이지.
+    </h1>
+  );
+};
+export default AnalysisOverviewPage;
