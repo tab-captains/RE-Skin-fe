63f74952645c97f6a48522a826da19af69a5c78a
kiwi13ird <rbf060114@gmail.com>
2025-11-16 16:07:38 +0900

    design:mainPage 날씨 컴포넌트 구성 완료

--- src/features/main/components/WeatherCard.jsx
+++ src/features/main/components/WeatherCard.jsx
@@ -15,39 +15,49 @@ const WeatherCard = ({weather=weatherMock}) => {
   return(
     <>
     <Wrapper>
-      <TextWrapper>
+      <TopSection>
+      <TopTextWrapper>
         <Text>현재 기온</Text>
         <Temp>{weather.temperature}°C</Temp>
         <Text>{weather.city}</Text>
-      </TextWrapper>
+      </TopTextWrapper>
     <GaugesWrapper>
       <GaugeCircle label="미세먼지" value={weather.pm10} max={150} />
       <GaugeCircle label="UV 지수" value={weather.uvIndex} max={11} />
       <GaugeCircle label="습도" value={weather.humidity} max={100} />
     </GaugesWrapper>
+    </TopSection>
+    <BottomText>오늘은 미세먼지가 높으니 꼼꼼한 클렌징 잊지 마세요!</BottomText>
     </Wrapper>
-    <Text>오늘은 미세먼지가 높으니 꼼꼼한 클렌징 잊지 마세요!</Text>
     </>
   );
 };
+const Wrapper = styled.div`
+  width: 600px;
+  height: 170px;
+  padding: 20px 30px;
+  border-radius: 15px;
+  border: 1px solid ${colors.weather};
+  background-color:${colors.weather};
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+const TopSection =styled.div`
+  display: flex;
+  justify-content: space-between;
+`
 
-const TextWrapper =styled.div`
+const TopTextWrapper =styled.div`
   display: flex;
   flex-direction: column;
   gap: 5px;
 `
-const Wrapper = styled.div`
-  width: 600px;
-  height: 190px;
-  padding: 20px 30px;
-  border-radius: 15px;
-  border: 1px solid;
-  border-color: ${colors.weather};
-  background-color:${colors.weather};
-  display: flex;
-  justify-content: space-between;
+const BottomText = styled.div`
+  font-size: 1rem;
+  color: white;
+  margin: 10px 0;
 `;
-
 const Text = styled.div`
   font-size: 0.8rem;
   color: white;
